83b6f7cc181759b1aa532e0cc0790d9d348e1572
Macitron <117962989+Macitron@users.noreply.github.com>
2024-12-02 17:13:05 -0800

    Bash day 1 part 2 solution
    my god that's tiny

--- 1/part1.sh
+++ 1/part1.sh
@@ -3,7 +3,13 @@
 # nth column: cat input | awk '{print $n}'
 # sorted columns side-by-side: paste <(cat input | awk '{print $1}' | sort) <(cat input | awk '{print $2}' | sort)
 
-paste <(cat input | awk '{ print $1 }' | sort) <(cat input | awk '{ print $2 }' | sort) \
+echo -n "Distance: "
+paste <(awk '{ print $1 }' input | sort) <(awk '{ print $2 }' input | sort) \
     | awk '{print ($2 - $1)}'   \
     | sed -E 's/^-//g'          \
     | awk '{s+=$1} END {print s}'
+
+echo -n "Similarity Score: "
+join <(awk '{print $1}' input | sort) <(awk '{print $2}' input | sort) \
+    | uniq -c   \
+    | awk '{s+=($1 * $2)} END {print s}'
