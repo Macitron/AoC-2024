a185a9743adfaf5f3bed3ab5c104e917d106a85e
Macitron <117962989+Macitron@users.noreply.github.com>
2024-12-04 00:14:14 -0800

    awk lmfao

--- day03/sln.sh
+++ day03/sln.sh
@@ -6,9 +6,7 @@ if [[ -z $infile ]]; then
     exit 1
 fi
 
-sum=0
-while read -r mul; do
-    (( sum += $(sed -E 's/.*\(([0-9]{1,3}),([0-9]{1,3})\)/\1 * \2/g' <<< "$mul") ))
-done < <(grep -oE 'mul\([0-9]{1,3},[0-9]{1,3}\)' "$infile")
-
-echo "Mul sum: $sum"
+echo -n "Mul sum: "
+grep -oE 'mul\([0-9]{1,3},[0-9]{1,3}\)' "$infile"           \
+    | sed -E 's/.*\(([0-9]{1,3}),([0-9]{1,3})\)/\1 \2/g'    \
+    | awk '{s+=($1 * $2)} END {print s}'
