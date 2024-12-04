18489fece304cd5b6cf14caf3cba982f1c53fa75
Macitron <117962989+Macitron@users.noreply.github.com>
2024-12-04 00:01:58 -0800

    herestring lol

--- day03/sln.sh
+++ day03/sln.sh
@@ -8,7 +8,7 @@ fi
 
 sum=0
 while read -r mul; do
-    (( sum += $(echo "$mul" | sed -E 's/.*\(([0-9]{1,3}),([0-9]{1,3})\)/\1 * \2/g') ))
+    (( sum += $(sed -E 's/.*\(([0-9]{1,3}),([0-9]{1,3})\)/\1 * \2/g' <<< "$mul") ))
 done < <(grep -oE 'mul\([0-9]{1,3},[0-9]{1,3}\)' "$infile")
 
 echo "Mul sum: $sum"
